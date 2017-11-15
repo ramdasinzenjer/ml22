e38339128474c0efb7c2781f14613f6ea7811fbf
ramdas <ramdasinfoinzenjer@gmail.com>
2017-11-15 13:42:48 +0530

    almost completed

--- app/src/main/java/com/example/lida/myapplication/feedbacksucc.java
+++ app/src/main/java/com/example/lida/myapplication/feedbacksucc.java
@@ -1,5 +1,6 @@
 package com.example.lida.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -13,5 +14,13 @@ public class feedbacksucc extends AppCompatActivity {
         getSupportActionBar().hide();
         RelativeLayout ll = (RelativeLayout) findViewById(R.id.lltt);
         ll.setAlpha((float) 0.4);
+
+
+    }
+    @Override
+    public  void onBackPressed()
+    {
+        Intent i = new Intent(feedbacksucc.this,Comp.class);
+        startActivity(i);
     }
 }
